4bf83a08ee02e9afbaf644cc09ada2c182ccfcfe
Anna Carolina Zomer <117862465+z0mer@users.noreply.github.com>
2024-10-20 19:39:41 -0300

    Update QueriesAlgebraRelacional.sql

--- QueriesAlgebraRelacional.sql
+++ QueriesAlgebraRelacional.sql
@@ -60,4 +60,92 @@ GROUP BY d.id_disco, d.titulo
 HAVING COUNT(m.id_musica) > 2;
 
 -- Quais são os nomes dos artistas que têm discos lançados antes de 2010 e que têm músicas na playlist 'Top 50'?
+SELECT DISTINCT a.nome
+FROM Artista a
+JOIN Disco d ON a.id_artista = d.id_artista
+JOIN Musica m ON d.id_disco = m.id_disco
+JOIN artista_musica ma ON m.id_musica = ma.id_musica
+JOIN Playlist_Musica pm ON m.id_musica = pm.id_musica
+JOIN Playlist pl ON pm.id_playlist = pl.id_playlist
+WHERE d.data_lancamento < '2010-01-01'
+  AND pl.titulo = 'Top 50';
 
+-- Quais músicas são interpretadas por mais de um artista?
+SELECT m.titulo
+FROM Musica m
+JOIN artista_musica ma ON m.id_musica = ma.id_musica
+GROUP BY m.id_musica, m.titulo
+HAVING COUNT(ma.id_artista) > 1;
+
+-- Liste os títulos das músicas que aparecem em mais de uma playlist.
+SELECT m.titulo
+FROM Musica m
+JOIN Playlist_Musica pm ON m.id_musica = pm.id_musica
+GROUP BY m.id_musica, m.titulo
+HAVING COUNT(pm.id_playlist) > 1;]
+
+-- Encontre os nomes dos usuários que têm playlists que incluem a música 'Bohemian Rhapsody'.
+SELECT DISTINCT u.nome
+FROM Usuario u
+JOIN Playlist pl ON u.id_usuario = pl.id_usuario
+JOIN Playlist_Musica pm ON pl.id_playlist = pm.id_playlist
+JOIN Musica m ON pm.id_musica = m.id_musica
+WHERE m.titulo = 'Bohemian Rhapsody';
+
+-- Qual é o título da música mais longa do disco 'Dark Side of the Moon'?
+  -- Não temos nenhum disco chamado "Dark Side of the Moon", então deixamos o código configurado para mostrar
+  -- o disco '1989'.
+SELECT m.titulo
+FROM Musica m
+JOIN Disco d ON m.id_disco = d.id_disco
+WHERE d.titulo = '1989'
+ORDER BY m.duracao_segundos DESC
+LIMIT 1;
+
+-- Liste todos os discos lançados por um artista específico em um determinado ano.
+SELECT d.titulo
+FROM Disco d
+WHERE d.id_artista = 4
+  AND EXTRACT(YEAR FROM d.data_lancamento) = 2015;
+
+-- Quais são os nomes dos artistas que têm músicas em playlists criadas por um usuário específico?
+SELECT DISTINCT a.nome
+FROM Artista a
+JOIN artista_musica ma ON a.id_artista = ma.id_artista
+JOIN Musica m ON ma.id_musica = m.id_musica
+JOIN Playlist_Musica pm ON m.id_musica = pm.id_musica
+JOIN Playlist pl ON pm.id_playlist = pl.id_playlist
+WHERE pl.id_usuario = 1;
+
+-- Encontre a lista de músicas que não estão em nenhuma playlist.
+SELECT m.titulo
+FROM Musica m
+LEFT JOIN Playlist_Musica pm ON m.id_musica = pm.id_musica
+WHERE pm.id_musica IS NULL;
+
+-- Liste os títulos das músicas e os nomes dos artistas que têm mais de 3 músicas em uma mesma playlist.
+SELECT m.titulo, a.nome
+FROM Musica m
+JOIN artista_musica ma ON m.id_musica = ma.id_musica
+JOIN Artista a ON ma.id_artista = a.id_artista
+JOIN Playlist_Musica pm ON m.id_musica = pm.id_musica
+GROUP BY m.id_musica, m.titulo, a.nome
+HAVING COUNT(pm.id_playlist) > 3;
+
+-- Quais são os discos que contêm músicas de artistas que têm pelo menos 2 discos lançados?
+SELECT DISTINCT d1.titulo
+FROM Disco d1
+JOIN Musica m ON d1.id_disco = m.id_disco
+JOIN artista_musica ma ON m.id_musica = ma.id_musica
+JOIN Artista a ON ma.id_artista = a.id_artista
+JOIN Disco d2 ON a.id_artista = d2.id_artista
+GROUP BY d1.id_disco, d1.titulo
+HAVING COUNT(d2.id_disco) >= 2;
+
+-- Liste todos os usuários e suas playlists, mas apenas para playlists que contêm pelo menos 5 músicas?
+SELECT u.nome AS Usuario, pl.titulo AS Playlist
+FROM Usuario u
+JOIN Playlist pl ON u.id_usuario = pl.id_usuario
+JOIN Playlist_Musica pm ON pl.id_playlist = pm.id_playlist
+GROUP BY u.id_usuario, u.nome, pl.id_playlist, pl.titulo
+HAVING COUNT(pm.id_musica) >= 5;
